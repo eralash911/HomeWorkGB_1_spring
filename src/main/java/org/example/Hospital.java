cdef5991316ae5d35933c0a322411be78572147d
eralash911 <unreal725@gmail.com>
2022-09-21 03:20:49 +0300

    dependecy with setter

--- src/main/java/org/example/Hospital.java
+++ src/main/java/org/example/Hospital.java
@@ -6,7 +6,14 @@ public class Hospital {
     //@Autowired
     private Human human;
 
+    public Hospital() {
+    }
+
     public Hospital(Human human) {
+        this.human = human;
+    }
+
+    public void setHuman(Human human) {
         this.human = human;
     }
 

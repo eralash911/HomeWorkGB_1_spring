692e8cec225fb93b0602b77587991a4f538fe42d
eralash911 <unreal725@gmail.com>
2022-09-21 03:26:43 +0300

    added properties:name and vol

--- src/main/java/org/example/Hospital.java
+++ src/main/java/org/example/Hospital.java
@@ -5,6 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class Hospital {
     //@Autowired
     private Human human;
+    private String name;
+    private  int vol;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVol() {
+        return vol;
+    }
+
+    public void setVol(int vol) {
+        this.vol = vol;
+    }
 
     public Hospital() {
     }
